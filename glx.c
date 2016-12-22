a2cad74cc4fa805c57c6fc490fd68776be9b28ce
Qiang Yu <yuq825@gmail.com>
2016-12-22 23:05:46 +0800

    add glx_query_version

--- glx.c
+++ glx.c
@@ -7,9 +7,26 @@
 #include <X11/Xproto.h>
 #include <GL/glxproto.h>
 
+static int glx_query_version(struct client *client, void *req)
+{
+	const xGLXQueryVersionReq *r = req;
+	xGLXQueryVersionReply reply = {
+		.type = X_Reply,
+		.sequenceNumber = client->sequence_number,
+		.length = 0,
+		.majorVersion = 1,
+		.minorVersion = 4,
+	};
+	return client_write(client, &reply, sz_xGLXQueryVersionReply, NULL, 0);
+}
+
 static int glx_ext_handler(struct client *client, void *req)
 {
 	const xReq *r = req;
+
+	if (r->data == X_GLXQueryVersion)
+		return glx_query_version(client, req);
+
 	printf("%d: glx null req code=%d len=%d\n", client->fd, r->data, r->length);
 	return xproto_error(client, BadRequest, 0, r->reqType, r->data);
 }
