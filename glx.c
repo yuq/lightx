90fa9dccae91c80a0d53003560d6ae215119e564
Qiang Yu <yuq825@gmail.com>
2016-12-23 22:31:27 +0800

    add glx_query_server_string for GLX_VERSION

--- glx.c
+++ glx.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <lightx/client.h>
 #include <lightx/xproto.h>
+#include <lightx/misc.h>
 
 #include <X11/X.h>
 #include <X11/Xproto.h>
@@ -20,6 +22,33 @@ static int glx_query_version(struct client *client, void *req)
 	return client_write(client, &reply, sz_xGLXQueryVersionReply, NULL, 0);
 }
 
+static int glx_query_server_string(struct client *client, void *req)
+{
+	const xGLXQueryServerStringReq *r = req;
+	const char *ptr;
+	size_t n, len;
+
+	switch (r->name) {
+	case GLX_VERSION:
+		ptr = "1.4";
+        break;
+	default:
+		return xproto_error(client, BadValue, 0, r->reqType, r->glxCode);
+	}
+
+	n = strlen(ptr) + 1;
+	len = pad_to_int32(n);
+	char buf[len + sz_xGLXQueryVersionReply];
+	xGLXQueryServerStringReply *reply = (xGLXQueryServerStringReply *)buf;
+	reply->type = X_Reply;
+	reply->sequenceNumber = client->sequence_number;
+	reply->length = len >> 2;
+	reply->n = n;
+	memcpy(reply + 1, ptr, n);
+
+	return client_write(client, buf, sz_xGLXQueryVersionReply + len, NULL, 0);
+}
+
 static int glx_ext_handler(struct client *client, void *req)
 {
 	const xReq *r = req;
@@ -27,6 +56,8 @@ static int glx_ext_handler(struct client *client, void *req)
 	switch (r->data) {
 	case X_GLXQueryVersion:
 		return glx_query_version(client, req);
+	case X_GLXQueryServerString:
+		return glx_query_server_string(client, req);
 	}
 
 	printf("%d: glx null req code=%d len=%d\n", client->fd, r->data, r->length);
